03c7b94bf6c3cf30768658a225fdd09d3f98a6bf
Eric García Pérez <suericgp@gmail.com>
2014-03-25 10:01:20 -0600

    Initial Commit

---
+++ MPI/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MPI
+//
+//  Created by sueric on 25/03/14.
+//  Copyright (c) 2014 SuEric. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
